ac7c5ef33a62524fc7956a212ad339aebb65bf27
Bob <paul@paulandrenae.com>
2018-03-23 16:02:21 +0000

    clean & build working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,87 @@
 module.exports = function(grunt) {
 
-  // Project configuration
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    webserver: 'C:/apps/Apache Software Foundation/Apache24/htdocs/',
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        webserver: 'C:/apps/Apache Software Foundation/Apache24/htdocs/',
     
-    copy: {
-        css: {
-            files: [{
-                expand: true,
-                cwd: 'src/css',
-                src: '**',
-                dest: 'build/css'
-            }]
+        clean: {
+            build: ['build']
         },
-        test: {
-            files: [{
-                expand: true,
-                cwd: 'build',
-                src: '**', // read everything inside the cwd
-                dest: '<%= webserver %>/'
-            }]
-        }
-    },
+        
+        copy: {
+            css: {
+                files: [
+                    {src: 'node_modules/bootstrap/dist/css/bootstrap.min.css', dest: 'build/css/', expand: true, flatten: true}
+                ]
+            },
+            js: {
+                files: [
+                    {src: [                        
+                        'node_modules/jquery/dist/jquery.min.js', 
+                        'node_modules/popper/dist/popper.min.js',         
+                        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+                    ], dest: 'build/js/', expand: true, flatten: true}
+                ]                            
+            },
+            html: {
+                files: [
+                    {src: 'src/*.html', dest: 'build/', expand: true, flatten: true}
+                ]
+            }            
+        }, // copy
                     
-    uglify: {
-        options: {
-            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-        },
-        js: {
-            src: 'src/js/*.js',
-            dest: 'build/js/<%= pkg.name %>.min.js'
-        }
-    },
+        concat: {
+            css: {                
+                src: 'src/css/*.css',
+                dest: 'build/css/<%= pkg.name %>.css'
+            }
+        }, // concat
     
-    watch: {
-        css: {
-            files: ['css/**'],
-            tasks: ['copy:css']
-        },
-        js: {
-            files: ['js/**'],
-            tasks: ['uglify']
-        }        
-    }   
-  });
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            js: {
+                src: 'src/js/*.js',
+                dest: 'build/js/<%= pkg.name %>.min.js'
+            }
+        }, // uglify
+    
+        watch: {
+            css: {
+                files: ['src/css/*.css'],
+                tasks: ['concat:css']
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: ['uglify']
+            },
+            html: {
+                files: ['src/*.html'],
+                tasks: ['copy:html']
+            }        
+        } // watch
+    }); // initConfig
 
-  // Load plugin tasks
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load plugin tasks  
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // My tasks
-  grunt.registerTask('test', function() {
-    console.log('Testing 1 2 3');
-  });
+    // My tasks
+    grunt.registerTask('test', function() {
+        console.log('Testing 1 2 3');
+    });
   
-  // Default task
-  grunt.registerTask('default', ['copy:css', 'uglify', 'watch']);
+    // Default task
+    grunt.registerTask('default', [
+        'clean',
+        'copy', 
+        'concat', 
+        'uglify', 
+        'watch'
+    ]);
 };
